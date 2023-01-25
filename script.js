88bdbeb47f7c7ae60ca8c269dea18b1ffc6836b3
Vieiravs2 <matheusemidio.canal@gmail.com>
2023-01-25 04:15:03 -0300

    Feat: Style and principal function finalized

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ const caracteres = '1234567890-=qwertyuiop´[asdfghjklç~]zxcvbnm,.;/!@#$%¨&*()
 const inputCaracteres = document.querySelector('#gerar-senha');
 const inputSenhaGerada = document.querySelector('#senha-gerada');
 const btnGerarSenha = document.querySelector('#click-gerar');
+const mensagemDeErro = document.querySelector('#error-message');
 let senha;
 
 const gerarSenha = () => {
-  senha = ''
+  senha = '';
 
   if (!inputCaracteres.value) {
-    alert('Você precisa adicionar a quantidade de caracteres')
+    alert('Você precisa adicionar a quantidade de caracteres');
     return
   }
+
+  if (inputCaracteres.value > 40) {
+    inputCaracteres.value = 40;
+    mensagemDeErro.innerText = 'O máximo de caracteres que podem ser gerados é 40.';
+    setTimeout(() => {
+      mensagemDeErro.innerText = ''
+    }, 2000);
+  }
+
   
   for (let index = 0; index < inputCaracteres.value; index += 1) {
     let stringAleatorio = Math.floor(Math.random() * caracteres.length);
